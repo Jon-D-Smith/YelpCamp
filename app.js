6c9a7084d954dd874bc069b2bb95f2c9fcd2db32
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-08 20:28:39 -0500

    Added basic detail/show page

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground })
+})
+
 app.listen(port, () => {
     console.log(`Connected on port ${port}`);
 })
