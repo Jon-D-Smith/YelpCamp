03596ca1324e8eb83c78ae66d2e25cb14c63b8e7
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-10 10:56:54 -0500

    Added ejs-mate and setup boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const port = 3000;
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override')
 //DB Model and setup
 const mongoose = require('mongoose');
@@ -19,6 +20,7 @@ db.once("open", () => {
 
 const app = express();
 //setting View engine and template engine
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
