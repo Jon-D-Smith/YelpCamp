40e2be99da9d6a240ddeb314836d519fdb700861
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-14 18:40:46 -0500

    Refactored error handling schema

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = 3000;
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const joi = require('joi');
+const { campgroundSchema } = require('./schemas.js')
 const methodOverride = require('method-override')
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
@@ -12,7 +12,6 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const { STATUS_CODES } = require('http');
 const { error } = require('console');
-const Joi = require('joi');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -37,6 +36,19 @@ app.use(methodOverride('_method'));
 
 
 
+const validateCampground = (req, res, next) => {
+
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+
+}
+
+
 //Home Page
 app.get('/', (req, res) => {
     res.render('home')
@@ -51,23 +63,9 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid campground data', 400)
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    })
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    console.log(result)
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -90,7 +88,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
