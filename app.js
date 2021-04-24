ffe0f46fe590626efddb09d44f255d7b1f35f32c
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-24 13:18:06 -0500

    Created campground routes file to slim down app file

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useCreateIndex: true,
     useUnifiedTopology: true
 });
+
+//Routes
+const campgrounds = require('./routes/campgrounds')
+
+
 //DB Checking for errors
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
@@ -37,17 +42,7 @@ app.use(methodOverride('_method'));
 
 
 
-const validateCampground = (req, res, next) => {
 
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-
-}
 
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
@@ -60,57 +55,12 @@ const validateReview = (req, res, next) => {
 }
 
 
+app.use('/campgrounds', campgrounds)
 //Home Page
 app.get('/', (req, res) => {
     res.render('home')
 })
-//All campgrounds
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index', { campgrounds })
-}))
-//Creating Campgrounds
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new')
-})
 
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid campground data', 400)
-
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-
-}))
-
-
-//Campground detail page
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews');
-    res.render('campgrounds/show', { campground })
-}))
-
-//Campground edit pages
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', { campground })
-
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-//Campground delete route
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-}))
 
 
 // Review Routing
