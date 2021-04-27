4fce7318777f067c4b4d010c204a773b986024da
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-27 17:49:35 -0500

    Finished passport local setup. Created test fake user

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
 
 
 //DB Model and setup
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
+const User = require('./models/user');
 const { STATUS_CODES } = require('http');
 const { error } = require('console');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -59,9 +63,18 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
 app.use(session(sessionConfig));
 app.use(flash());
 
+//Authentication through passport
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //Flash middleware
 app.use((req, res, next) => {
@@ -69,6 +82,12 @@ app.use((req, res, next) => {
     res.locals.error = req.flash('error');
     next();
 })
+
+// app.get('/fakeuser', async (req, res) => {
+//     const user = new User({ email: "Jon@gmail.com", username: 'Jon' });
+//     const newUser = await User.register(user, "chicken");
+//     res.send(newUser);
+// })
 
 //App use routes
 app.use('/campgrounds', campgrounds);
