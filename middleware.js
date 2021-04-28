0a74c4b53724277dae98af4529860cf61485d5fc
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-28 17:59:06 -0500

    Added a returnTo URL to return user to their desired page upon login

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
 
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be signed in to submit new campgrounds')
         return res.redirect('/login');
     }
