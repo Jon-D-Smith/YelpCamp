589f085166f7060b35d5a667c207633b8d64f248
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-29 18:16:54 -0500

    Basic login auth set for reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const { validateReview } = require('../middleware.js')
+const { validateReview, isLoggedIn } = require('../middleware.js')
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError')
 
@@ -10,10 +10,11 @@ const ExpressError = require('../utils/ExpressError')
 
 // Review Routing
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     const review = await new Review(req.body.review)
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
