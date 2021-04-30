5c00e362bfb57e1d1f3c5cf00f546b2afe8c9675
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-30 18:16:45 -0500

    grouped routes by prefix

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,23 +5,23 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware.js')
 const Campground = require('../models/campground');
 
+// Index route methods
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-//All campgrounds
-router.get('/', catchAsync(campgrounds.index))
-
-//Creating Campgrounds
+//New campground form
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-//Campground detail page
-router.get('/:id', catchAsync(campgrounds.showCampground))
+//Specific campground route methods
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+
 
 //Campground edit pages
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-
-//Campground delete route
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 module.exports = router;
