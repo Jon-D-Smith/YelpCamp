2f38842f0a387a84634301b7ff76ae080df50b33
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-02 16:45:43 -0500

    Fixed image loading on the front page

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,19 @@ const seedDB = async () => {
             author: '6088b66ea42e3c00582b41a7',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+
+                    url: 'https://res.cloudinary.com/dzxzufbpu/image/upload/v1619906449/YelpCamp/tq4xuioeko1nt3rfszht.jpg',
+                    filename: 'YelpCamp/tq4xuioeko1nt3rfszht'
+                },
+                {
+
+                    url: 'https://res.cloudinary.com/dzxzufbpu/image/upload/v1619906451/YelpCamp/mkjtrwyt9s7yekarw9h1.jpg',
+                    filename: 'YelpCamp/mkjtrwyt9s7yekarw9h1'
+                }
+
+            ],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             price
         })
